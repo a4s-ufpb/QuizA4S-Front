1d717e76dfdc3988f21c95c5127a6968f6fcd6f0
RonyAbreu <ronyelison78@gmail.com>
2024-05-24 20:10:47 -0300

    Refactor: corrigindo url da api

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { useFooter } from "./hooks/useFooter";
 // Css
 import "./App.css";
 
-export const URL_BASE = "http://api.observatorioturismopb.com.br:8085/api/v1";
+export const URL_BASE = "https://quizapp.a4s.dev.br/api/v1";
 export const DEFAULT_IMG = "https://t3.ftcdn.net/jpg/04/60/01/36/360_F_460013622_6xF8uN6ubMvLx0tAJECBHfKPoNOR5cRa.jpg";
 
 function App() {
