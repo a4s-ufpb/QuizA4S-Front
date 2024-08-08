f8e25d0dc6f822fb752d8c909bdc146684c7b25f
RonyAbreu <ronyelison78@gmail.com>
2024-08-08 09:30:07 -0300

    Refactor: utilizando variáveis de ambiente

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ import { useFooter } from "./hooks/useFooter";
 // Css
 import "./App.css";
 
-export const URL_BASE = "https://quizapp.a4s.dev.br/api/v1";
-export const DEFAULT_IMG = "https://t3.ftcdn.net/jpg/04/60/01/36/360_F_460013622_6xF8uN6ubMvLx0tAJECBHfKPoNOR5cRa.jpg";
+export const URL_BASE = import.meta.env.VITE_URL_API;
+export const DEFAULT_IMG = import.meta.env.VITE_DEFAULT_IMG_URL;
 
 function App() {
   const { isAuthenticated, loading } = useContext(AuthenticationContext);
